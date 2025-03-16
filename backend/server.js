a39cb1344ffd69159bedb1ffeb46d5c5d57e6d8b
Titanop242424 <optitan317@gmail.com>
2025-03-16 21:53:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -83,6 +83,6 @@ app.post('/start-attack', (req, res) => {
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://45.77.193.174:${PORT}`);
 });
